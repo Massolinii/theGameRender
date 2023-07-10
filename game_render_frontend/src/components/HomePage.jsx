36d6b508a58254e1760c1a2dc841d1d7f283b69c
Massolinii <massimilianoesposito548@gmail.com>
2023-07-10 23:08:48 +0200

    Working on hero

--- game_render_frontend/src/components/HomePage.jsx
+++ game_render_frontend/src/components/HomePage.jsx
@@ -4,7 +4,7 @@ import Hero from "./Hero";
 function HomePage() {
   return (
     <div>
-      <h1>Homepage</h1>
+      <h2>Futura Navbar</h2>
       <Hero />
       <h2>Categorie:</h2>
       <a href="/category/1">Clicca qui per la Categoria 1</a>
